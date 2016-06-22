0ae5af8fdd4af6ae822d53c23552331b10e203c0
trossman2016 <zacknafien2000@yahoo.com>
2016-06-22 02:08:27 +0000

    Add files via upload

--- Calculations.cs
+++ Calculations.cs
@@ -1,14 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Calculations
+namespace HelperItems
 {
-	public static bool CalculateHit(int ac, int thaco, int roll)
-	{
-        bool wasHit = false;
-        if (roll > thaco + (ac * -1))
+    public class Calculations
+    {
+        /// <summary>
+        /// Calculates whether or not a list of d20 rolls would hit a specified
+        /// armor class based off of the characters needed value to hit armor class 0.
+        /// </summary>
+        /// <param name="rolls">The rolls.</param>
+        /// <param name="thac0">The thac0.</param>
+        /// <param name="ac">The ac.</param>
+        /// <typeparam name="wasHit">The boolean value for whether or not a hit was successful.</typeparam>
+        /// <returns>Returns a <typeparamref name="wasHit"/> list.</returns>
+        public static List<bool> CalculateHit(List<int> rolls, int thac0, int ac)
         {
-            wasHit = true;
+            List<bool> wasHit = new List<bool>();
+            foreach (int roll in rolls)
+            {
+                if (roll >= thac0 + (ac * -1) || roll == 20)
+                {
+                    wasHit.Add(true);
+                }
+                else
+                {
+                    wasHit.Add(false);
+                }
+            }
+            return wasHit;
         }
-        return wasHit;
     }
 }
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CharacterSheet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CharacterObject.CharStats charStats = new CharacterObject.CharStats();
+        Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StatsGrid.DataContext = charStats;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            charStats.Strength = charStats.Strength.RollCharacterStats();
+            charStats.Dexterity = charStats.Dexterity.RollCharacterStats();
+            charStats.Constitution = charStats.Constitution.RollCharacterStats();
+            charStats.Intelligence = charStats.Intelligence.RollCharacterStats();
+            charStats.Wisdom = charStats.Wisdom.RollCharacterStats();
+            charStats.Charisma = charStats.Charisma.RollCharacterStats();
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        static Random rnd = new Random();
+
+        public static string RollCharacterStats(this string obj)
+        {
+            List<int> Rolls = DiceRoller.Dice.dieroll(6, 4, rnd, true);
+            int total = 0;
+            Rolls.Sort();
+
+            foreach (int roll in Rolls)
+            {
+                if (Rolls.IndexOf(roll) > 0)
+                {
+                    total += roll;
+                }
+                //MessageBox.Show(roll.ToString());
+            }
+            return total.ToString();
+        }
+    }
+}
