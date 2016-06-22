4349abb4c323f3971f47aeddcf209be218e78679
trossman2016 <zacknafien2000@yahoo.com>
2016-06-22 02:02:51 +0000

    Add files via upload

---
+++ Calculations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Calculations
+{
+	public static bool CalculateHit(int ac, int thaco, int roll)
+	{
+        bool wasHit = false;
+        if (roll > thaco + (ac * -1))
+        {
+            wasHit = true;
+        }
+        return wasHit;
+    }
+}
